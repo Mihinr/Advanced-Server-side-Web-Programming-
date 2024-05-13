7c1548e5f7d784a99f1f0c16f0070ef340039704
Mihin Ratnayake <85722458+Mihinr@users.noreply.github.com>
2024-05-13 13:06:59 +0530

    Update postQuestion.php

--- application/views/postQuestion.php
+++ application/views/postQuestion.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/postQuestions.css');?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/postQuestion.css');?>">
     <title>Post Question</title>
 
     <!-- Include jQuery -->
